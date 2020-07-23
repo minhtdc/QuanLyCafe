8d6b79f9dcc1caeab27b78c9ddb75a880b35fc9d
minhtdc <67999471+minhtdc@users.noreply.github.com>
2020-07-23 22:23:32 +0700

    Initial Commit

---
+++ QuanLyCafe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuanLyCafe
+//
+//  Created by Nguyễn Minh on 7/23/20.
+//  Copyright © 2020 Nguyễn Minh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
